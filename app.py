0bcef66b3f201c45be1cc3f5545b0e956986eb26
Shruti-Bhosale2 <101860065+Shruti-Bhosale2@users.noreply.github.com>
2026-01-12 06:52:40 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "id": "03d37fa9-2959-44bf-aa9d-52f444cfab4a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
