4c6b0552c39442dfcc7b7cc936330db0222f9e77
Shruti-Bhosale2 <101860065+Shruti-Bhosale2@users.noreply.github.com>
2026-01-14 22:35:16 -0800

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ac4e732f-0590-44a9-b7db-b855188eeb15",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
